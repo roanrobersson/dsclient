f862473b2ac19cd7ad6d94add44e105d227e430b
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-10-19 23:51:47 -0300

    Clients pagination

--- src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -4,6 +4,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,6 +29,20 @@ public class ClientResource {
 	
 	@Autowired
 	private ClientService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		
+		Pageable pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
+		// DESCOBRIR PORQUE DÁ PRA RETORNAR O ResponseEntity<> com objeto ClientDTO ou também Page<> 
+		return ResponseEntity.ok().body(list);
+	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
--- src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,11 @@ public class ClientService {
 	
 	@Autowired
 	private ClientRepository repository;
+	
+	public Page<ClientDTO> findAllPaged(Pageable pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
 	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
